9a2dc6f49df8a758c9881c024e319335add5f9ea
Nicola Mazzucato <nicola.mazzucato@arm.com>
2026-03-23 15:02:24 +0000

    [zep noup][tf-m-test] ARMCLANG.cmake: Remove -fshort-enums and -fshort-wchar options
    When RTX and NS are built without the -fshort options,
    a linking error appears:
    Error: L6242E: Cannot link object xyz.o as its attributes are
    incompatible with the image attributes.
       ... wchart-16 clashes with wchart-32.
       ... packed-enum clashes with enum_is_int.
    
    Remove those -fshort flags while other other components
    are also built without such flags.
    
    Signed-off-by: Nicola Mazzucato <nicola.mazzucato@arm.com>

--- api-tests/tools/cmake/compiler/ARMCLANG.cmake
+++ api-tests/tools/cmake/compiler/ARMCLANG.cmake
@@ -64,6 +64,6 @@ elseif(${CPU_ARCH} STREQUAL armv8a)
     set(TARGET_SWITCH "-march=armv8-a")
 endif()
 
-set(CMAKE_C_FLAGS              "--target=arm-arm-none-eabi ${TARGET_SWITCH} -g -Wall -Werror -Wextra -fshort-enums -fshort-wchar -funsigned-char -fdata-sections -ffunction-sections -mno-unaligned-access -mfpu=none")
+set(CMAKE_C_FLAGS              "--target=arm-arm-none-eabi ${TARGET_SWITCH} -g -Wall -Werror -Wextra -funsigned-char -fdata-sections -ffunction-sections -mno-unaligned-access -mfpu=none")
 set(CMAKE_ASM_FLAGS            "${TARGET_SWITCH} -mthumb")
 set(CMAKE_EXE_LINKER_FLAGS     "--strict --map --symbols --xref  --info=summarysizes,sizes,totals,unused,veneers --diag_warning=L6204")
